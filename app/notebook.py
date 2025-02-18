0d0eb80a45bcf6b99126543e906811b49dbbb5ab
Susana Morales <sarazasusana2@gmail.com>
2025-02-18 11:30:38 -0500

    Se inicia clase Notebook

--- app/notebook.py
+++ app/notebook.py
@@ -5,11 +5,26 @@ class Note:
     HIGH: str = "HIGH"
     MEDIUM: str = "MEDIUM"
     LOW: str = "LOW"
+
     def __init__(self, code: str, title: str, text: str, importance: str):
      self.code = code
      self.title = title
      self.text = text
      self.importance = importance
-     self.creation_date = datetime
-     self.tags = list[str]
+     self.creation_date = datetime.now()
+     self.tags = []
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self):
+        return f"Date: {self.creation_date}/n{self.title}: {self.text}"
+
+
+class Notebook:
+
+
+
+
 
